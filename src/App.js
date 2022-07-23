df3595610ed87d736fe99855529ed47c6595909f
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2022-07-22 22:32:49 -0500

    Background for div
    optional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Menu from "./GameScripts/Menu";
-import SinglePlayer from "./GameScripts/SinglePlayer";
+import Menu from "./components/GameScripts/Menu";
+import SinglePlayer from "./components/GameScripts/SinglePlayer";
 import style from "./style.css";
 
 export default function App() {
--- src/components/GameScripts/Menu.js
+++ src/components/GameScripts/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 
+import MenuList from "/MenuComponents";
 export default function Menu(props) {
   const page = <div className="Menu-background"></div>;
   console.log("called menu");
