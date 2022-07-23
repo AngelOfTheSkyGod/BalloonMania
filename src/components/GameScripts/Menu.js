89de5de6019749f7168278bc09109cb4c3a98820
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2022-07-22 22:59:14 -0500

    Adding styling to menu
    Starting on the Menu. For some reason, the MenuList folder isn't being accessed.

--- src/components/GameScripts/Menu.js
+++ src/components/GameScripts/Menu.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
 
-import MenuList from "/MenuComponents";
+//import MenuList from "/components/MenuComponents/MenuList";
 export default function Menu(props) {
-  const page = <div className="Menu-background"></div>;
+  const page = (
+    <div className="Menu-background">
+      <h1 className="Menu-title">Balloon Pop Mania</h1>
+      {/* <MenuList /> */}
+    </div>
+  );
   console.log("called menu");
   return page;
 }
