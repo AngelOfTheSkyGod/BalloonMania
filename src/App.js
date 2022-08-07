07d21b756d378a313eb4a045d6db75651e05eaa9
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2022-08-06 22:04:59 -0500

    balloon pop function && revert history function made them interact with the react components and update everything well. next step is making a css grid be generated from the info given.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import Menu from "./components/GameScripts/Menu";
 import SinglePlayer from "./components/GameScripts/SinglePlayer.js";
 
-import { makeGrid, floatUp } from "./components/GameScripts/Grid";
+import {
+  makeGrid,
+  floatUp,
+  popBalloon,
+  revertHistory,
+} from "./components/GameScripts/Grid";
 import style from "./style.css";
-
+let coordinate = { row: -1, col: -1 };
 export default function App() {
   const [mode, setMode] = React.useState("Menu");
   const [showMenu, setShowMenu] = React.useState({
@@ -17,13 +22,63 @@ export default function App() {
 
   const [game, setGame] = React.useState({
     history: [
-      { board: makeGrid([], 7, 7), points: 0, time: 30000, rows: 7, cols: 7 },
+      {
+        board: makeGrid([], 7, 7),
+        points: 0,
+        time: 30000,
+        rows: 7,
+        cols: 7,
+        popped: 0,
+      },
     ],
     currentBoard: 0,
   });
 
   const menuCooldown = React.useRef(false);
 
+  // (gameObject, row, col)
+  React.useEffect(() => {
+    function keyListener(event) {
+      if (!isNaN(event.key) && coordinate.col === -1) {
+        if (coordinate.row === -1) {
+          coordinate.row = event.key;
+        } else {
+          coordinate.col = event.key;
+        }
+      } else if (event.key === "Enter" || coordinate.col !== -1) {
+        setGame((oldBoard) => {
+          let board = popBalloon(
+            game,
+            parseInt(coordinate.row),
+            parseInt(coordinate.col)
+          );
+          if (board != null) {
+            return board;
+          }
+          return oldBoard;
+        });
+
+        coordinate.row = -1;
+        coordinate.col = -1;
+      } else if (event.key.toLowerCase() === "z") {
+        setGame((oldBoard) => {
+          let board = revertHistory(game);
+          if (board != null) {
+            return board;
+          }
+          return oldBoard;
+        });
+      }
+      console.log(`coordinate: (${coordinate.row}, ${coordinate.col})`);
+    }
+    // Update the document title using the browser API
+    document.addEventListener("keypress", keyListener);
+
+    return () => {
+      console.log("deleted event.");
+      document.removeEventListener("keypress", keyListener);
+    };
+  }, []);
   //
   return (
     (mode === "Menu" && (
--- src/components/GameScripts/Grid.js
+++ src/components/GameScripts/Grid.js
@@ -1,5 +1,22 @@
+/*
+the balloons are 5 colors. air is empty.
+*/
 const colors = ["^", "&", "%", "#", "*"];
-function printArray(grid, rows, cols) {
+/*GAME OBJECT: 
+{
+    history: [
+      { board: makeGrid([], 7, 7), points: 0, time: 30000, rows: 7, cols: 7 },
+    ],
+    currentBoard: 0,
+  }
+*/
+/*GRID OBJECT: 
+{ board: makeGrid([], 7, 7), points: 0, time: 30000, rows: 7, cols: 7 }
+*/
+/*
+print array is just for printing our matrix.
+*/
+function printMatrix(grid, rows, cols) {
   let gridString = "";
   for (let r = 0; r < rows; r++) {
     let str = "";
@@ -10,10 +27,14 @@ function printArray(grid, rows, cols) {
   }
   return gridString;
 }
+/*
+makes a board of random colored balloon objects. the board is rows x cols
+it'll have air spots which are empty slots where there's
+no balloons. the balloons must be on top and air at the bottom.
+*/
 export function makeGrid(grid, rows, cols) {
   for (let r = 0; r < rows; r++) {
     let array = [];
-    let str = "";
     for (let c = 0; c < cols; c++) {
       let balloon = {
         color: colors[Math.floor(Math.random() * colors.length)],
@@ -21,17 +42,17 @@ export function makeGrid(grid, rows, cols) {
         row: r,
         col: c,
       };
-      str += ` ${balloon.color}\n`;
       array.push(balloon);
     }
     grid.push(array);
   }
-  console.log(`BEFORE EDITING: \n${printArray(grid, rows, cols)}`);
   grid = floatUp(grid, rows, cols);
-  console.log(`AFTER EDITING: \n${printArray(grid, rows, cols)}`);
+  console.log(`created matrix:\n${printMatrix(grid, rows, cols)}`);
   return grid;
 }
-// board: makeGrid([], 7, 7), points: 0, time: 30000, rows: 7, cols: 7
+
+// makes all balloons float up if there's any air above them.
+//balloons must float up to the top because they're balloons.
 export function floatUp(grid, rows, cols) {
   for (let c = 0; c < cols; c++) {
     let breakSpot = -1;
@@ -39,9 +60,9 @@ export function floatUp(grid, rows, cols) {
     let fixingArray = [];
     /////for grabbing all the unique balloons
     for (let r = 0; r < rows; r++) {
-      if (grid[r][c].color == "*" && breakSpot == -1) {
+      if (grid[r][c].color === "*" && breakSpot === -1) {
         breakSpot = r;
-      } else if (grid[r][c].color != "*" && breakSpot != -1) {
+      } else if (grid[r][c].color !== "*" && breakSpot !== -1) {
         broken = true;
         fixingArray.push(grid[r][c]);
       }
@@ -61,4 +82,85 @@ export function floatUp(grid, rows, cols) {
     }
   }
   return grid;
+}
+
+function outOfBounds(gameObject, row, col) {
+  let grid = gameObject.history[gameObject.currentBoard].board;
+  if (row >= grid.rows || col >= grid.cols || row < 0 || col < 0) {
+    return true;
+  }
+  return false;
+}
+
+function rPop(gameObject, row, col, prevBalloon, popped) {
+  let board = gameObject.history[gameObject.currentBoard].board;
+  if (
+    outOfBounds(gameObject, row, col) ||
+    board[row][col].color !== board[prevBalloon.r][prevBalloon.c].color ||
+    board[row][col] === colors[4] ||
+    board[row][col].popped === true
+  ) {
+    return popped;
+  }
+  popped++;
+  board[row][col].popped = true;
+
+  if (row - 1 !== prevBalloon.r) {
+    popped = rPop(gameObject, row - 1, col, { r: row, c: col }, popped);
+  }
+  if (row + 1 !== prevBalloon.r) {
+    popped = rPop(gameObject, row + 1, col, { r: row, c: col }, popped);
+  }
+  if (col - 1 !== prevBalloon.c) {
+    popped = rPop(gameObject, row, col - 1, { r: row, c: col }, popped);
+  }
+  if (col + 1 !== prevBalloon.c) {
+    popped = rPop(gameObject, row, col + 1, { r: row, c: col }, popped);
+  }
+
+  board[row][col].color = colors[4];
+
+  return popped;
+}
+
+export function popBalloon(gameObject, row, col) {
+  let board = gameObject.history[gameObject.currentBoard];
+  if (
+    outOfBounds(gameObject, row, col) ||
+    board.board[row][col].color === colors[4]
+  ) {
+    return null;
+  }
+
+  gameObject.history.push(JSON.parse(JSON.stringify(board)));
+  gameObject.currentBoard++;
+  board = gameObject.history[gameObject.currentBoard];
+  console.log(
+    `current board: \n${printMatrix(board.board, board.rows, board.cols)}`
+  );
+  let balloonsPopped = rPop(gameObject, row, col, { r: row, c: col }, 0);
+
+  console.log(`popped: ${balloonsPopped} balloons!`);
+
+  console.log(
+    `board after: \n${printMatrix(board.board, board.rows, board.cols)}`
+  );
+  board.popped = balloonsPopped;
+  return gameObject;
+}
+
+export function revertHistory(gameObject, row, col) {
+  if (gameObject.currentBoard <= 0) {
+    return null;
+  }
+  gameObject.history.pop();
+  gameObject.currentBoard--;
+  console.log(
+    `rewinding history one step: \n${printMatrix(
+      gameObject.history[gameObject.currentBoard].board,
+      gameObject.history[gameObject.currentBoard].rows,
+      gameObject.history[gameObject.currentBoard].cols
+    )} current history: ${gameObject.currentBoard}`
+  );
+  return gameObject;
 }
