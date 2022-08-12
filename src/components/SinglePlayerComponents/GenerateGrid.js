44dac2e0433e71cb1b9de6e461c7b88f0fca3152
Angel <79809585+AngelOfTheSkyGod@users.noreply.github.com>
2022-08-12 11:48:01 -0500

    Balloon popping Added balloon popping that pops a balloon at a time recursively every .25 seconds. Added score and combo display :D

--- src/components/SinglePlayerComponents/GenerateGrid.js
+++ src/components/SinglePlayerComponents/GenerateGrid.js
@@ -2,26 +2,53 @@ import React from "react";
 import { Icon } from "@iconify/react";
 import { GiAirBalloon } from "react-icons/gi";
 
+import {
+  makeGrid,
+  floatUp,
+  popBalloon,
+  revertHistory,
+} from "../GameScripts/Grid";
+
 // const colors = ["^", "&", "%", "#", "*"];
 const colors = {
   "^": { color: "blue" },
   "&": { color: "red" },
   "%": { color: "green" },
-  "#": { color: "green" },
+  "#": { color: "yellow" },
   "*": { opacity: "0%", color: "blue" },
 };
+
+function onClick(row, column, game, setGame) {
+  console.log(`popped: (${row}, ${column})`);
+  popBalloon(game, row, column, setGame);
+}
 export default function GenerateGrid(props) {
-  console.log(props);
+  console.log("current version: ", props.props.game.currentBoard);
   let rows = props.props.game.history[props.props.game.currentBoard].board.map(
     (row) => {
       let balloons = row.map((balloon) => {
         return (
-          <div className="Balloon" role="button">
+          <div
+            className="Balloon"
+            role="button"
+            row={balloon.row}
+            column={balloon.column}
+            popped={balloon.popped}
+            onClick={() =>
+              onClick(
+                balloon.row,
+                balloon.col,
+                props.props.game,
+                props.props.setGame
+              )
+            }
+          >
             <Icon
               icon="bi:balloon-fill"
               style={colors[balloon.color]}
               width="70%"
               height="80%"
+              className="balloon-image"
             />
           </div>
         );
